ee401297d461e8a5457b8de65f88a2a6aaf9c595
Antony S Mechery <antonysmechery25@gmail.com>
2026-02-07 23:33:48 +0530

    Delete server directory

--- server/routes/AuthRouter.js
+++
@@ -1,9 +0,0 @@
-const { signup, login, googleLogin } = require('../controllers/AuthController');
-const { signupValidation, loginValidation } = require('../middleware/AuthValidation');
-const router = require('express').Router();
-
-router.post('/login', loginValidation, login);
-router.post('/signup', signupValidation, signup);
-router.post('/google-login', googleLogin);
-
-module.exports = router;
